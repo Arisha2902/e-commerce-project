c45b78bbc611ebd688619964d8d9548d67fe54bd
Arisha Firoz <169101584+Arisha2902@users.noreply.github.com>
2024-12-24 08:55:17 +0530

    ok

--- shopy/authen/views.py
+++ shopy/authen/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render,redirect,HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+from .utils import TokenGenerator, generate_token 
+from django.utils.encoding import force_bytes
+from django.core.mail import EmailMessage
 
 # Create your views here.
 def signup(request):
@@ -11,19 +16,27 @@ def signup(request):
         if password !=confirm_password:
             messages.warning(request, " Password is not matching")
             return render(request, 'signup.html')
-            return HttpResponse("password incorrectt")
-        
-        
         try:
             if User.objects.get(username=email):
-              return  HttpResponse("email already exist ")
-
-                # return render(request, 'auth/signup.html')
-            
+                messages.warning(request, " Email is taken")
+                return render(request, 'signup.html')
+                 
+            #   return  HttpResponse("email already exist ")
         except Exception as identifiers:
             pass
         user = User.objects.create_user( email,email,password)
+        user.is_active = False
         user.save()
+        email_subject = "Activate your account"
+        message = render_to_string( 'activate.html', {
+            'user': user,
+            # 'domain': get_current_site(request).domain,
+            'domain': '127.0.0.1:8000' ,
+            'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+            'token': generate_token.make_token(user)
+
+        })
+
         return HttpResponse("user created")
 
     return render(request, "signup.html")
