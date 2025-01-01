428ccb5d029100aed3333666fceb3798828c62cf
Arisha Firoz <169101584+Arisha2902@users.noreply.github.com>
2025-01-01 13:37:55 +0530

    `Added checkout functionality and updated models, views, and templates accordingly.`

--- shopy/app/admin.py
+++ shopy/app/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from app.models import Contact,product
+from app.models import Contact,Orders,OrderUpdate,product
 
 admin.site.register(Contact)
 admin.site.register(product)
+admin.site.register(Orders) 
+admin.site.register(OrderUpdate)
 
 # Register your models here.
--- shopy/app/urls.py
+++ shopy/app/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
       path('',views.index, name='index'),
       path('contact',views.contact, name='contact'),
       path('about',views.about, name='about'),
+      path('checkout/',views.checkout, name='checkout'),
+      path('handlerequest/', views.handlerequest, name='HandleRequest'),
       # path('',views., name='')
 
 
